54db0a909cbacdd29697a30b25faf10196d93f14
Dan Yerushalmi <dan@irpsystems.com>
2019-11-04 17:58:49 +0200

    added support for binary messages http and to log

--- src/main/java/com/narfster/LogController.java
+++ src/main/java/com/narfster/LogController.java
@@ -1,5 +1,6 @@
 package com.narfster;
 
+import java.io.InputStream;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Map;
@@ -32,6 +33,7 @@ public class LogController {
 		res += String.format("'%s' = %s\r\n", "protocol", request.getProtocol());
 		res += String.format("'%s' = %s\r\n", "path", request.getRequestURI());
 		res += String.format("'%s' = %s\r\n", "method", request.getMethod());
+		res += String.format("'%s' = %s\r\n", "content-type", request.getContentType());
 
 		for (Map.Entry<String, String> entry : headers.entrySet()) {
 			res += String.format("'%s' = %s\r\n", entry.getKey(), entry.getValue());
@@ -41,9 +43,30 @@ public class LogController {
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-MMM-yyyy HH:mm:ss");
 		LocalDateTime now = LocalDateTime.now();
 		String time = dtf.format(now);
-
+		
 		logService.addLogMessage(new LogModel(time, res, s));
+		
+		return new ResponseEntity<String>(HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/api/blog2", method = { RequestMethod.POST })
+	public ResponseEntity<String> binary_log(@RequestHeader Map<String, String> headers, HttpServletRequest request,
+			@RequestBody byte[] byteArr ) {
 
+		String s = bytesToHex(byteArr);
+		String res = new String("");
+		
+		// debug
+		System.out.println(s);
+
+		// Get current date time
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-MMM-yyyy HH:mm:ss");
+		LocalDateTime now = LocalDateTime.now();
+		String time = dtf.format(now);
+		
+		logService.addLogMessage(new LogModel(time, res, s));
+		
+		
 		return new ResponseEntity<String>(HttpStatus.OK);
 	}
 
@@ -75,5 +98,22 @@ public class LogController {
 
 		return new ResponseEntity<String>(String.format(res, headers.size()), HttpStatus.OK);
 	}
+	
+	private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
+	public static String bytesToHex(byte[] bytes) {
+	    char[] hexChars = new char[bytes.length * 2 + bytes.length ];
+	    for (int j = 0; j < bytes.length; j++) {
+	        int v = bytes[j] & 0xFF;
+	        
+	        hexChars[j * 3] = HEX_ARRAY[v >>> 4];
+	        hexChars[j * 3 + 1] = HEX_ARRAY[v & 0x0F];
+	        
+	        // add whitespace
+	        hexChars[j * 3 + 2] = ' ';
+	        
+	    }
+	    return new String(hexChars);
+	}
+	
 
 }
